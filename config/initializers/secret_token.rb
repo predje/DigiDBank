1ac230405f0425a108076642c24f84c93943e981
nick <n.c.venderbosch@gmail.com>
2012-12-27 17:09:44 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DigiD::Application.config.secret_token = 'b6f49f77fdd8d8c540f00f95b3e77bb2c463950712b7bebd54f0d442a6c8654335424be603d1ad0a63ee5867e464d8538c3123b5fd921e5de4b48e9cc57a3f36'
